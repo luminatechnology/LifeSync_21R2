63df9ee0f9ad92e0230e2b57d5b1280c43ff0136
林子瑋 <JO3.JO3@hotmail.com>
2022-11-08 13:26:56 +0800

    1.Stock Item 新增AMBomCostHistory 畫面欄位

---
+++ LUMCustomization/LUMCustomization/Graph_Extensions/InventoryItemMaint_Extension.cs
@@ -0,0 +1,20 @@
+﻿using PX.Data;
+using PX.Data.BQL.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Objects.AM;
+
+namespace PX.Objects.IN
+{
+    public class InventoryItemMaint_Extension : PXGraphExtension<InventoryItemMaint>
+    {
+        public SelectFrom<AMBomCostHistory>
+              .Where<AMBomCostHistory.inventoryID.IsEqual<InventoryItem.inventoryID.FromCurrent>
+                .And<AMBomCostHistory.startDate.IsEqual<InventoryItem.stdCostDate.FromCurrent>>
+                .And<AMBomCostHistory.isDefaultBom.IsEqual<True>>>
+              .View BomCostHistory;
+    }
+}
